19c6b29d98d36405b39cb9ed9215beb72b393080
ValeriaHall <Probsnotkirby@gmail.com>
2026-06-04 15:05:42 -0400

    Add shared configuration

---
+++ shared.lua
@@ -0,0 +1,19 @@
+-- Shared configuration for TV script
+
+Config = {}
+
+-- TV Models available
+Config.TVModels = {
+    "v_res_m_tv",
+    "v_res_d_tv",
+    "v_res_tv01",
+    "v_res_tv02"
+}
+
+-- Supported video platforms (YouTube, Twitch, etc.)
+Config.SupportedPlatforms = {
+    "youtube",
+    "twitch",
+    "vimeo",
+    "custom"
+}
