9d08568064da849a3782190c5de93b11a303bcad
ValeriaHall <Probsnotkirby@gmail.com>
2026-06-04 15:32:11 -0400

    Update to play video only on targeted screen

--- client.lua
+++ client.lua
@@ -1,8 +1,9 @@
 local isPlaying = false
 local tvUrl = ""
+local targetedEntity = nil
 
 -- Function to play TV/Device
-function PlayDevice(url)
+function PlayDevice(url, entity)
     if not url or url == "" then
         TriggerEvent('chat:addMessage', {
             args = {"Screen", "^1Invalid URL provided!^7"},
@@ -11,7 +12,16 @@ function PlayDevice(url)
         return
     end
 
+    if not entity or not DoesEntityExist(entity) then
+        TriggerEvent('chat:addMessage', {
+            args = {"Screen", "^1Invalid screen!^7"},
+            color = {255, 0, 0}
+        })
+        return
+    end
+
     tvUrl = url
+    targetedEntity = entity
     isPlaying = true
     
     TriggerEvent('chat:addMessage', {
@@ -19,12 +29,12 @@ function PlayDevice(url)
         color = {0, 255, 0}
     })
 
-    -- Sync with server
-    TriggerServerEvent('tvscript:syncTVStream', url, true)
+    -- Sync with server - only this specific entity
+    TriggerServerEvent('tvscript:syncTVStream', url, true, entity)
 end
 
 -- Function to pause device
-function PauseDevice()
+function PauseDevice(entity)
     if not isPlaying then
         TriggerEvent('chat:addMessage', {
             args = {"Screen", "^1Nothing is playing!^7"},
@@ -38,11 +48,11 @@ function PauseDevice()
         color = {255, 165, 0}
     })
 
-    TriggerServerEvent('tvscript:syncTVStream', tvUrl, false)
+    TriggerServerEvent('tvscript:syncTVStream', tvUrl, false, entity)
 end
 
 -- Function to stop device
-function StopDevice()
+function StopDevice(entity)
     if not isPlaying then
         TriggerEvent('chat:addMessage', {
             args = {"Screen", "^1Nothing is playing!^7"},
@@ -53,13 +63,14 @@ function StopDevice()
 
     isPlaying = false
     tvUrl = ""
+    targetedEntity = nil
 
     TriggerEvent('chat:addMessage', {
         args = {"Screen", "^1Stopped^7"},
         color = {255, 0, 0}
     })
 
-    TriggerServerEvent('tvscript:syncTVStream', "", false)
+    TriggerServerEvent('tvscript:syncTVStream', "", false, entity)
 end
 
 -- Add target option for TV props
@@ -70,7 +81,7 @@ if GetResourceState('ox_target') == 'started' then
             label = '📺 Play Video',
             icon = 'fa-solid fa-play',
             onSelect = function(data)
-                TriggerEvent('tvscript:openInputDialog')
+                TriggerEvent('tvscript:openInputDialog', data.entity)
             end
         },
         {
@@ -78,7 +89,7 @@ if GetResourceState('ox_target') == 'started' then
             label = '⏸ Pause',
             icon = 'fa-solid fa-pause',
             onSelect = function(data)
-                PauseDevice()
+                PauseDevice(data.entity)
             end
         },
         {
@@ -86,7 +97,7 @@ if GetResourceState('ox_target') == 'started' then
             label = '⏹ Stop',
             icon = 'fa-solid fa-stop',
             onSelect = function(data)
-                StopDevice()
+                StopDevice(data.entity)
             end
         }
     })
@@ -120,7 +131,7 @@ end
 
 -- Input dialog event
 RegisterNetEvent('tvscript:openInputDialog')
-AddEventHandler('tvscript:openInputDialog', function()
+AddEventHandler('tvscript:openInputDialog', function(entity)
     -- For ox_target with built-in input
     if GetResourceState('ox_lib') == 'started' then
         local input = exports.ox_lib:inputDialog('Play Video', {
@@ -133,7 +144,7 @@ AddEventHandler('tvscript:openInputDialog', function()
         })
         
         if input then
-            PlayDevice(input[1])
+            PlayDevice(input[1], entity)
         end
     else
         -- Fallback to chat command
@@ -145,13 +156,13 @@ AddEventHandler('tvscript:openInputDialog', function()
 end)
 
 RegisterNetEvent('tvscript:pauseTV')
-AddEventHandler('tvscript:pauseTV', function()
-    PauseDevice()
+AddEventHandler('tvscript:pauseTV', function(entity)
+    PauseDevice(entity)
 end)
 
 RegisterNetEvent('tvscript:stopTV')
-AddEventHandler('tvscript:stopTV', function()
-    StopDevice()
+AddEventHandler('tvscript:stopTV', function(entity)
+    StopDevice(entity)
 end)
 
 -- Fallback commands
@@ -164,25 +175,27 @@ RegisterCommand('tvplay', function(source, args, rawCommand)
         })
         return
     end
-    PlayDevice(url)
+    PlayDevice(url, targetedEntity)
 end, false)
 
 RegisterCommand('tvpause', function(source, args, rawCommand)
-    PauseDevice()
+    PauseDevice(targetedEntity)
 end, false)
 
 RegisterCommand('tvstop', function(source, args, rawCommand)
-    StopDevice()
+    StopDevice(targetedEntity)
 end, false)
 
 -- Handle server events
 RegisterNetEvent('tvscript:updateTV')
-AddEventHandler('tvscript:updateTV', function(url, playing)
+AddEventHandler('tvscript:updateTV', function(url, playing, entity)
     if playing and url ~= "" then
         isPlaying = true
         tvUrl = url
+        targetedEntity = entity
     else
         isPlaying = false
         tvUrl = ""
+        targetedEntity = nil
     end
 end)
