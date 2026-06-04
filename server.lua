3466ca88f6e998800b03a2d584d687cec29db773
ValeriaHall <Probsnotkirby@gmail.com>
2026-06-04 15:28:56 -0400

    Simplify server script for global broadcast

--- server.lua
+++ server.lua
@@ -5,17 +5,17 @@ local tvState = {
 
 -- Handle TV stream sync from client
 RegisterServerEvent('tvscript:syncTVStream')
-AddEventHandler('tvscript:syncTVStream', function(url, playing, deviceEntity)
+AddEventHandler('tvscript:syncTVStream', function(url, playing)
     tvState.isPlaying = playing
     tvState.url = url
 
     -- Broadcast to all clients
-    TriggerClientEvent('tvscript:updateTV', -1, url, playing, deviceEntity)
+    TriggerClientEvent('tvscript:updateTV', -1, url, playing)
 end)
 
 -- Provide initial state to newly joined players
 AddEventHandler('playerConnecting', function()
     if tvState.isPlaying then
-        TriggerClientEvent('tvscript:updateTV', source, tvState.url, tvState.isPlaying, nil)
+        TriggerClientEvent('tvscript:updateTV', source, tvState.url, tvState.isPlaying)
     end
 end)
