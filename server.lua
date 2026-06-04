62f5b62072e35a3b4e029c401bf6762ce043a519
ValeriaHall <Probsnotkirby@gmail.com>
2026-06-04 15:32:30 -0400

    Update server to track individual screen entity

--- server.lua
+++ server.lua
@@ -1,21 +1,23 @@
 local tvState = {
     isPlaying = false,
-    url = ""
+    url = "",
+    entity = nil
 }
 
 -- Handle TV stream sync from client
 RegisterServerEvent('tvscript:syncTVStream')
-AddEventHandler('tvscript:syncTVStream', function(url, playing)
+AddEventHandler('tvscript:syncTVStream', function(url, playing, entity)
     tvState.isPlaying = playing
     tvState.url = url
+    tvState.entity = entity
 
-    -- Broadcast to all clients
-    TriggerClientEvent('tvscript:updateTV', -1, url, playing)
+    -- Broadcast to all clients with entity info
+    TriggerClientEvent('tvscript:updateTV', -1, url, playing, entity)
 end)
 
 -- Provide initial state to newly joined players
 AddEventHandler('playerConnecting', function()
     if tvState.isPlaying then
-        TriggerClientEvent('tvscript:updateTV', source, tvState.url, tvState.isPlaying)
+        TriggerClientEvent('tvscript:updateTV', source, tvState.url, tvState.isPlaying, tvState.entity)
     end
 end)
