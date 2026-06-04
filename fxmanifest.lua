eb415b541dfc0a5fd5e84ca2153e30caa0b3ff09
ValeriaHall <Probsnotkirby@gmail.com>
2026-06-04 15:05:20 -0400

    Add fxmanifest configuration

---
+++ fxmanifest.lua
@@ -0,0 +1,25 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Your Name'
+description 'TV Script for FiveM'
+version '1.0.0'
+
+lua54 'yes'
+
+shared_scripts {
+    'shared.lua'
+}
+
+client_scripts {
+    'client.lua'
+}
+
+server_scripts {
+    'server.lua'
+}
+
+dependencies {
+    '/server:5181',
+    '/onesync'
+}
