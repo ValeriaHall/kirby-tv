caba10157e9d76197b1b5ec5713fd61bff83515e
ValeriaHall <Probsnotkirby@gmail.com>
2026-06-04 15:08:16 -0400

    Update server script with device entity support

--- server.lua
+++ server.lua
@@ -5,17 +5,17 @@ local tvState = {
 
 -- Handle TV stream sync from client
 RegisterServerEvent('tvscript:syncTVStream')
-AddEventHandler('tvscript:syncTVStream', function(url, playing)
+AddEventHandler('tvscript:syncTVStream', function(url, playing, deviceEntity)
     tvState.isPlaying = playing
     tvState.url = url
 
     -- Broadcast to all clients
-    TriggerClientEvent('tvscript:updateTV', -1, url, playing)
+    TriggerClientEvent('tvscript:updateTV', -1, url, playing, deviceEntity)
 end)
 
 -- Provide initial state to newly joined players
 AddEventHandler('playerConnecting', function()
     if tvState.isPlaying then
-        TriggerClientEvent('tvscript:updateTV', source, tvState.url, tvState.isPlaying)
+        TriggerClientEvent('tvscript:updateTV', source, tvState.url, tvState.isPlaying, nil)
     end
 end)
